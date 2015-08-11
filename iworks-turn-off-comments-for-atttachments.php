7e02f9ece1858dac8c1775c452c13a411add2579
Marcin Pietrzak <marcin@iworks.pl>
2015-08-11 06:30:21 +0200

    comments of for attachments

---
+++ iworks-turn-off-comments-for-atttachments.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Plugin Name: iWorks turn off comments for atttachments
+Plugin URI: http://iworks.pl/
+Description: turn off ability to comment attachments
+Version: 1.0
+Author: Marcin Pietrzak
+Author URI: http://iworks.pl/
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+class iworks_turn_off_comments_for_atttachments
+{
+    public function __construct()
+    {
+        add_action( 'comments_open', array( $this, 'comments_open' ), PHP_INT_MAX, 2 );
+    }
+    public static function init()
+    {
+        new iworks_turn_off_comments_for_atttachments();
+    }
+
+    public function comments_open($open, $post_id)
+    {
+        $post = get_post( $post_id );
+
+        if (
+            is_object($post)
+            && isset($post->post_type)
+            && 'attachment' == $post->post_type
+        ) {
+            $open = false;
+        }
+
+        return $open;
+    }
+
+}
+
+iworks_turn_off_comments_for_atttachments::init();
+
