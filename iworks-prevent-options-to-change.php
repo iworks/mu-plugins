d1f702cf492620759706727e1b7c2212513e7c9e
Marcin Pietrzak <marcin@iworks.pl>
2021-05-17 08:11:11 +0200

    prevent

---
+++ iworks-prevent-options-to-change.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: iWorks prevent some options to change
+Plugin URI:
+Description: Prevent some WordPress options to change.
+Version: 1.0.0
+Author: Marcin Pietrzak
+Author URI: http://iworks.pl/
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+class iworks_prevent_options_to_change {
+	public function __construct() {
+
+		add_filter( 'option_users_can_register', '__return_false' );
+		add_filter( 'option_default_role', array( $this, 'option_default_role' ), 10, 2 );
+	}
+
+	public function option_default_role( $value, $option ) {
+		return 'subscriber';
+	}
+}
+
+new iworks_prevent_options_to_change;
+
+
+
