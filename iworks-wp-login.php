6ae6f76b0c54aade6cf60938cf3d4097668a9b4e
Marcin Pietrzak <marcin@iworks.pl>
2021-06-10 07:12:52 +0200

    update

--- iworks-wp-login.php
+++ iworks-wp-login.php
@@ -3,7 +3,7 @@
 Plugin Name: iWorks wp-login.php renamer
 Plugin URI: http://upprev.com/
 Description: raname wp-login.php o other name
-Version: 0.1
+Version: 1.0.1
 Author: Marcin Pietrzak
 Author URI: http://iworks.pl/
 License: GPLv2 or later
@@ -14,56 +14,52 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 /**
  * IMPORTANT!
  *
- * rename wp-login.php file to ${login_prefix}wp-login.php!!!
+ * 1. Make a symlink wp-login.php to ${login_prefix}wp-login.php!!!
+ * 2. Set 403 on wp-login.php file.
  */
 
 
-class iWorks_WP_Login_Redirect
-{
-    private $login_prefix = 'ephuiC0aoW-';
+class iWorks_WP_Login_Redirect {
 
-    public function __construct()
-    {
-        add_filter( 'site_url', array( &$this, 'site_url' ), 10, 2 );
-        add_filter( 'wp_redirect', array( &$this, 'wp_redirect' ), 10, 2 );
-        add_filter( 'lostpassword_url', array( &$this, 'lostpassword_url' ), 10, 2 );
-#        add_filter( 'network_site_url', array( &$this, 'network_site_url' ), 10, 3 );
-    }
+	private $login_prefix = 'ephuiC0aoW-';
 
-    public function site_url( $url, $file )
-    {
-        if ( preg_match( '/wp-login.php/', $file ) ) {
-            return preg_replace( '/wp-login/', $this->login_prefix.'wp-login', $url );
-        }
-        return $url;
-    }
+	public function __construct() {
+		add_filter( 'site_url', array( &$this, 'site_url' ), 10, 2 );
+		add_filter( 'wp_redirect', array( &$this, 'wp_redirect' ), 10, 2 );
+		add_filter( 'lostpassword_url', array( &$this, 'lostpassword_url' ), 10, 2 );
+		#        add_filter( 'network_site_url', array( &$this, 'network_site_url' ), 10, 3 );
+	}
 
-    public function wp_redirect( $location, $status )
-    {
-        if ( preg_match( '/^wp-login.php/', $location ) ) {
-            return preg_replace( '/wp-login/', $this->login_prefix.'wp-login', $location );
-        }
-        if ( preg_match( '@/wp-login.php@', $location ) ) {
-            return preg_replace( '@/wp-login@', '/'.$this->login_prefix.'wp-login', $location );
-        }
-        return $location;
-    }
+	public function site_url( $url, $file ) {
+		if ( preg_match( '/wp-login.php/', $file ) ) {
+			return preg_replace( '/wp-login/', $this->login_prefix . 'wp-login', $url );
+		}
+		return $url;
+	}
 
-    public function lostpassword_url( $lostpassword_url, $redirect )
-    {
-        if ( preg_match( '@/wp-login.php@', $lostpassword_url ) ) {
-            return preg_replace( '/\/wp-login/', '/'.$this->login_prefix.'wp-login', $lostpassword_url );
-        }
-        return $lostpassword_url;
-    }
+	public function wp_redirect( $location, $status ) {
+		if ( preg_match( '/^wp-login.php/', $location ) ) {
+			return preg_replace( '/wp-login/', $this->login_prefix . 'wp-login', $location );
+		}
+		if ( preg_match( '@/wp-login.php@', $location ) ) {
+			return preg_replace( '@/wp-login@', '/' . $this->login_prefix . 'wp-login', $location );
+		}
+		return $location;
+	}
 
-    public function network_site_url( $url, $path, $scheme )
-    {
-        if ( preg_match( '@/wp-login.php@', $url ) ) {
-            return preg_replace( '/\/wp-login/', '/'.$this->login_prefix.'wp-login', $url );
-        }
-        return $url;
-    }
+	public function lostpassword_url( $lostpassword_url, $redirect ) {
+		if ( preg_match( '@/wp-login.php@', $lostpassword_url ) ) {
+			return preg_replace( '/\/wp-login/', '/' . $this->login_prefix . 'wp-login', $lostpassword_url );
+		}
+		return $lostpassword_url;
+	}
+
+	public function network_site_url( $url, $path, $scheme ) {
+		if ( preg_match( '@/wp-login.php@', $url ) ) {
+			return preg_replace( '/\/wp-login/', '/' . $this->login_prefix . 'wp-login', $url );
+		}
+		return $url;
+	}
 
 }
 
