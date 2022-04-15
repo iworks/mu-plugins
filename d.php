81dac572de168e6a91f61cc7734f62480a6fcf43
Marcin Pietrzak <marcin@iworks.pl>
2022-04-15 11:48:44 +0200

    phpcs

--- d.php
+++ d.php
@@ -1,14 +1,13 @@
 <?php
 
-if ( !function_exists('d') ) {
-    function d( $a, $b = '' )
-    {
-        if (!is_array($a) ) {
-            $a= (array) $a;
-        }
-        echo '<pre>';
-        echo $b;
-        print_r( $a );
-        echo '</pre>';
-    }
+if ( ! function_exists( 'd' ) ) {
+	function d( $a, $b = '' ) {
+		if ( ! is_array( $a ) ) {
+			$a = (array) $a;
+		}
+		echo '<pre>';
+		echo $b;
+		print_r( $a );
+		echo '</pre>';
+	}
 }
